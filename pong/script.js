b1f7e88fc4474af29746951d50e41e3c9d7cc40a
Mederis Frambourt <mframbou@student.42nice.fr>
2022-07-15 16:42:51 +0200

    started doing profiles

--- pong/script.js
+++ pong/script.js
@@ -1,23 +1,33 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000});
-const cube = new THREE.Mesh(geometry, material);
 
-scene.add(cube);
+const p1 = new THREE.Mesh(new THREE.BoxGeometry(2, 22, 2), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+const p2 = new THREE.Mesh(new THREE.BoxGeometry(2, 22, 2), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+const ball = new THREE.Mesh(new THREE.SphereGeometry(1, 10, 10), new THREE.MeshBasicMaterial({ color: 0x0000ff }));
 
-camera.position.z = 20;
+scene.add(p1);
+scene.add(p2);
+scene.add(ball);
+
+p1.translateX(-30);
+p2.translateX(30);
+
+camera.position.z = 30;
+
+// ball.attach(camera);
+// ball.translateY(10);
+
 
 function animate() {
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
-	cube.rotateX(0.01);
-	cube.rotateY(0.01);
 }
 
 animate();
