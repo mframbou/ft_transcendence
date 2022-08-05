a02e9daeb95dda92902b5275e1514ee812e719e1
Mederis Frambourt <mframbou@student.42nice.fr>
2022-08-05 05:24:39 +0200

    Still learning svelt(kit)

--- nest-angular-app/frontend-svelte/src/App.svelte
+++ nest-angular-app/frontend-svelte/src/App.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import Button from "./Button.svelte"; // Could be any import name (like import Pouet from ./button.svelte)
+	// convention is to use uppercase for component names to differentiate from html tags
+
 	export let name: string;
 	let name2: string = "Pouet pouet";
 	let test: number = 1.5;
@@ -7,7 +10,7 @@
 	let value = 1.5;
 
 	let bgs = [
-		{name: 'dsamain', bgitude: 100, id: 1},
+		{name: 'dsamain', bgitude: 100, id: 0},
 		{name: 'oronda', bgitude: 5, id: 1}
 	];
 
@@ -33,7 +36,18 @@
 	{
 		pouet = e.target.value;
 	}
+
+	const removeBg = (id: number) =>
+	{
+		bgs = bgs.filter(bg => bg.id !== id);
+	}
+
 </script>
+
+<!--<Button></Button>-->
+<Button message="Click me bro"/>
+<Button isBlue={false} isRed={true}/>
+<!--	https://stackoverflow.com/questions/68051031/why-does-svelte-put-curly-braces-on-scalars-->
 
 <main>
 	<h1>Hello {name}! {name2} your iq is {test}</h1>
@@ -51,9 +65,19 @@
 
 	{#each bgs as bg , i (bg.id)} <!-- unique key at the end (https://svelte.dev/tutorial/keyed-each-blocks)	-->
 		<h3>{bg.name} (index {i}) has a bgitude of {bg.bgitude}</h3>
+		<button on:click={() => removeBg(bg.id)}>Remove</button>
+		<!-- because we cant call function with parameter (eg. cheerBg(bg.name)) directly we need to use expression instead of calling function directly -->
 	{:else }
 		<h3>No bgs found</h3>
 	{/each}
+
+	<!-- # = begin of control structure, : = other clause, / = end of control structure-->
+	{#if bgs.length > 0}
+		<h3>There are {bgs.length} bgs</h3>
+	{:else}
+		<h3>There are no bgs</h3>
+	{/if}
+
 </main>
 
 <style>
---
+++ nest-angular-app/frontend-svelte/src/Button.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let message = "This is my default value"; // export allows for modification from outside of the component
+	export let isBlue = true;
+	export let isRed = false;
+</script>
+
+<div>
+	<!--	Conditional classes depending on code condition -->
+	<button
+					class:red={isRed}
+					class:blue={isBlue}
+					on:click={() => {
+						isBlue = !isBlue;
+						isRed = !isRed;
+					}}
+	>{message}
+	</button>
+</div>
+
+<style>
+
+    .red {
+        background-color: red;
+        color: #ffffff;
+    }
+
+    .red:active {
+        background-color: red;
+    }
+
+    .blue {
+        background-color: blue;
+        color: white;
+    }
+
+    .blue:active {
+        background-color: blue;
+    }
+</style>
