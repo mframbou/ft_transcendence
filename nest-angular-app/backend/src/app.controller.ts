6a5ed44c7e1e68ce41f37d20f150cd4260ed863d
Salvatore Spina <saske0109@gmail.com>
2022-08-18 15:52:51 +0200

    app updated

--- nest-angular-app/backend/src/app.controller.ts
+++ nest-angular-app/backend/src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+// Nest
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+
+// Transcendence
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import ErrorDispatcher from './error-dispatcher/error-dispatcher';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {
+    this.prisma = new PrismaService();
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // https://www.youtube.com/watch?v=2n3xS89TJMI
+  @UseGuards(JwtAuthGuard)
+  @Get('user/:id')
+  async findOne(@Param('id') id: string): Promise<any> {
+    try {
+      return await this.prisma.user.findUnique({
+        where: {
+          idIntra: id,
+        },
+      });
+    } catch (e) {
+      ErrorDispatcher(e);
+    }
   }
 }
--- nest-angular-app/backend/src/app.module.ts
+++ nest-angular-app/backend/src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
+// Nest
+import { Module, CacheModule } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+
+// Transcendence
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { SessionModule } from './session/session.module';
+import { MySessionService } from './session/session.service';
+import { PermissionModule } from './permission/permission.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    SessionModule,
+    PermissionModule,
+    CacheModule.register({
+      store: redisStore,
+      host: 'rediStatus',
+      port: 6379,
+    }),
+  ],
+  providers: [AppService, MySessionService, AuthService, UserService],
 })
-export class AppModule { }
+export class AppModule {}
