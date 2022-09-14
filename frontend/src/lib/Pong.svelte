ae6acc31976e60486896453eda2aa1d12aa79d2d
Mederis Frambourt <mframbou@student.42nice.fr>
2022-09-14 15:51:44 +0200

    refactoring pong for online gameg

--- frontend/src/lib/Pong.svelte
+++ frontend/src/lib/Pong.svelte
@@ -1,286 +1,339 @@
 <script lang="ts">
 
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	import { pongSocketStore } from '$lib/stores';
 
 	export let isPlayerOne = true;
 
-	$pongSocketStore.on('onOpponentPaddleMove', (data) => {
+	$pongSocketStore.on('onOpponentPaddleMove', (data) =>
+	{
 		// console.log("OPPONENT PADDLE MOVE:", data);
 		com.y = data.y;
 	});
 
-	$pongSocketStore.on('onBallReset', (data) => {
-		console.log("BALL RESET:", data);
+	$pongSocketStore.on('onBallReset', (data) =>
+	{
+		console.log('BALL RESET:', data);
 		ball.velocityY = data.velocityY;
 		ball.velocityX = data.velocityX;
 
-		if (!isPlayerOne) {
+		if (!isPlayerOne)
+		{
 			ball.velocityX *= -1;
 		}
 
 		resetBall();
 	});
 
-	$pongSocketStore.on('onScoreChange', (data) => {
+	$pongSocketStore.on('onScoreChange', (data) =>
+	{
 		if (!data || data.player1Score === undefined || data.player2Score === undefined)
 			return;
 
-		console.log("SCORE CHANGE:", data);
+		console.log('SCORE CHANGE:', data);
 
 		if (isPlayerOne)
 		{
-			user.score = data.player1Score;
-			com.score = data.player2Score;
+			player1.score = data.player1Score;
+			player2.score = data.player2Score;
 		}
 		else
 		{
-			user.score = data.player2Score;
-			com.score = data.player1Score;
+			player1.score = data.player2Score;
+			player2.score = data.player1Score;
 		}
 		drawScore();
 	});
 
-	$pongSocketStore.on('onResetPaddles', (data) => {
-		user.x = 0;
-		user.y = canvas_height / 2 - paddle_height / 2;
+	$pongSocketStore.on('onResetPaddles', (data) =>
+	{
+		player1.paddle.x = 0;
+		player1.paddle.y = canvas_height / 2 - player1.paddle.height / 2;
 
-		com.x = canvas_width - paddle_width;
-		com.y = canvas_height / 2 - paddle_height / 2;
+		player2.paddle.x = canvas_width - player2.paddle.width;
+		player2.paddle.y = canvas_height / 2 - player2.paddle.height / 2;
 	});
 
-	let canvasElement:HTMLCanvasElement;
-	let context:CanvasRenderingContext2D;
+	let canvasElement: HTMLCanvasElement;
+	let context: CanvasRenderingContext2D;
 
-    onMount(async () =>
-    {
-			canvasElement.addEventListener("mousemove",movePaddle);
+	onMount(async () =>
+	{
+		canvasElement.addEventListener('mousemove', movePaddle);
 
-            if(canvasElement.getContext("2d") != null)
-                context = canvasElement.getContext("2d") as CanvasRenderingContext2D;
-            else
-                console.log("context is null");
-			
-            // pongSocket.on('', (data) => {
-            //     console.log('pongSocket received pong', data);
-            // });
+		if (canvasElement.getContext('2d') != null)
+			context = canvasElement.getContext('2d') as CanvasRenderingContext2D;
+		else
+			console.log('context is null');
 
-            // pongSocket.on('onStartGameReceive', (data) => {
-            //     console.log('pongSocket received pong', data);
-            // });
+		// pongSocket.on('', (data) => {
+		//     console.log('pongSocket received pong', data);
+		// });
 
-            // pongSocket.on('onStartGameReceive', (data) => {
-            //     console.log('pongSocket received pong', data);
-            // });
-			requestAnimationFrame(game);
-	})
+		// pongSocket.on('onStartGameReceive', (data) => {
+		//     console.log('pongSocket received pong', data);
+		// });
 
-    const computerLevel = 50;
+		// pongSocket.on('onStartGameReceive', (data) => {
+		//     console.log('pongSocket received pong', data);
+		// });
+		requestAnimationFrame(loop);
+	});
 
-    const canvas_width = 600;
-    const canvas_height = 400;
-    const paddle_width =  10;
-    const paddle_height = 100;
-    const paddle_color = "black";
+	const computerLevel = 50;
 
-
-    const framePerSecond = 50;
-
-    const user =
-        {
-            x : 0,
-            y : (canvas_height/2) - (paddle_height/2),
-            width : paddle_width,
-            height : paddle_height,
-            color : "blue",
-            score : 0
-        }
-    const com =
-        {
-            x : canvas_width - paddle_width,
-            y : (canvas_height/2) - (paddle_height/2),
-            width : paddle_width,
-            height : paddle_height,
-            color : "red",
-            score : 0,
-            random : 0
-        }
-
-    const net =
-        {
-            x : canvas_width/2 - 2,
-            y : 0,
-            width : 4,
-            height : 10,
-            color : "white",
-        }
+	const canvas_width = 600;
+	const canvas_height = 400;
+	const paddle_width = 10;
+	const paddle_height = 100;
+	const paddle_color = 'black';
 
 
-    const ball = {
-        x: canvas_width/2,
-        y: canvas_height/2,
-        radius: 10,
-        speed: 5,
-        velocityX: 5,
-        velocityY: 0,
-        color: "white",
-    }
+	const framePerSecond = 50;
 
-    function drawRect(x:number, y:number, w:number, h:number, color:string)
-    {
-        context.fillStyle = color;
-        context.fillRect(x,y,w,h);
-    }
+	interface IBall
+	{
+		x: number;
+		y: number;
+		radius: number;
+		velocityX: number;
+		velocityY: number;
+		speed: number;
+		color: string;
+	}
 
-    function drawCircle(x:number, y:number,r: number ,color:string)
-    {
-        context.fillStyle = color;
-        context.beginPath();
-        context.arc(x,y,r,Math.PI*2,0,false);
-        context.closePath();
-        context.fill();
+	interface IPaddle
+	{
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		color: string;
+	}
 
-    }
+	interface IPLayer
+	{
+		paddle: IPaddle;
+		score: number;
+	}
 
-    function drawText(text:string, x:number,y:number,color:string)
-    {
-        context.fillStyle = color;
-        context.font = "75px fantasy"
-        context.fillText(text,x,y);
-    }
+	const player1: IPLayer = {
+		paddle: {
+			x: 0,
+			y: canvas_height / 2 - paddle_height / 2,
+			width: paddle_width,
+			height: paddle_height,
+			color: 'blue',
+		},
+		score: 0
+	};
 
+	const player2: IPLayer = {
+		paddle: {
+			x: canvas_width - paddle_width,
+			y: canvas_height / 2 - paddle_height / 2,
+			width: paddle_width,
+			height: paddle_height,
+			color: 'red',
+		},
+		score: 0
+	};
 
-    function reset_canvas()
-    {
-        drawRect(0,0,600,400,"black");
-    }
+	const user =
+			{
+				x: 0,
+				y: (canvas_height / 2) - (paddle_height / 2),
+				width: paddle_width,
+				height: paddle_height,
+				color: 'blue',
+				score: 0
+			};
+	const com =
+			{
+				x: canvas_width - paddle_width,
+				y: (canvas_height / 2) - (paddle_height / 2),
+				width: paddle_width,
+				height: paddle_height,
+				color: 'red',
+				score: 0,
+				random: 0
+			};
 
-    function drawNet()
-    {
-        for(let i = 0; i < canvas_height; i+=20)
-            drawRect(net.x, net.y + i, net.width, net.height, net.color);
-    }
-
-    function drawBall()
-    {
-        drawCircle(ball.x, ball.y, ball.radius, ball.color);
-    }
-
-    function drawScore()
-    {
-        drawText(user.score.toString(),canvas_width/4,canvas_height/5,"white");
-        drawText(com.score.toString(),3*canvas_width/4,canvas_height/5,"white");
-    }
-
-    function resetBall()
-    {
-        ball.x = canvas_width/2;
-        ball.y = canvas_height/2;
-        ball.speed = 5;
-        ball.velocityX = -ball.velocityX;
-        com.random = 0;
-    }
-
-    function movePaddle(event:MouseEvent)
-    {
-
-        let rect = canvasElement.getBoundingClientRect();
-
-				$pongSocketStore.emit('onPaddleMove', {y: event.clientY - rect.top});
-
-        user.y = event.clientY - rect.top - user.height/2;
-        if(user.y + paddle_height > canvas_height)
-            user.y = canvas_height - paddle_height;
-        if(user.y < 0)
-            user.y = 0;
-         
-    }
-
-    function Update()
-    {
-        // com.y = ball.y -  com.height/2 + com.random;
-
-        if(com.y + paddle_height > canvas_height)
-            com.y = canvas_height - paddle_height;
-        if(com.y < 0)
-            com.y = 0;
-
-        if(ball.x + ball.radius > canvas_width)
-        {
-            // user.score++;
-						$pongSocketStore.emit('onPlayerScored', {score: user.score});
-            resetBall(); // reset so that next frame we dont accidentally resend message
-        }
-        else if(ball.x - ball.radius < 0)
-        {
-            // com.score++;
-
-						// opponent will emit itself so no need
-						// $pongSocketStore.emit('onOpponentScored', {score: com.score});
-
-            resetBall();
-        }
-
-        ball.x += ball.velocityX;
-        ball.y += ball.velocityY;
-
-        if(ball.y + ball.radius > canvas_height || ball.y - ball.radius < 0)
-        {
-            ball.velocityY = -ball.velocityY;
-        }
+	const net =
+			{
+				x: canvas_width / 2 - 2,
+				y: 0,
+				width: 4,
+				height: 10,
+				color: 'white',
+			};
 
 
-        let player = (ball.x < canvas_width/2) ? user : com;
-        if (collision(ball,player))
-        {
-            let collidPoint = (ball.y - (player.y + player.height/2));
-            collidPoint = collidPoint/(player.height/2);
+	const ball: IBall = {
+		x: canvas_width / 2,
+		y: canvas_height / 2,
+		radius: 10,
+		speed: 5,
+		velocityX: 5,
+		velocityY: 0,
+		color: 'white',
+	};
+
+	function drawPaddle(paddle: IPaddle)
+	{
+		context.fillStyle = paddle.color;
+		context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+	}
+
+	function drawRect(x: number, y: number, w: number, h: number, color: string)
+	{
+		context.fillStyle = color;
+		context.fillRect(x, y, w, h);
+	}
+
+	function drawCircle(x: number, y: number, r: number, color: string)
+	{
+		context.fillStyle = color;
+		context.beginPath();
+		context.arc(x, y, r, Math.PI * 2, 0, false);
+		context.closePath();
+		context.fill();
+
+	}
+
+	function drawText(text: string, x: number, y: number, color: string)
+	{
+		context.fillStyle = color;
+		context.font = '75px fantasy';
+		context.fillText(text, x, y);
+	}
 
 
-            let angleRad = (Math.PI/4) * collidPoint;
-            let direction = (ball.x < canvas_width/2) ? 1 : -1;
-            ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-            ball.velocityY = ball.speed * Math.sin(angleRad);
-            ball.speed += 0.2;
+	function reset_canvas()
+	{
+		drawRect(0, 0, 600, 400, 'black');
+	}
 
-            if(player == com)
-                com.random = Math.random()  * computerLevel ;
-        }
-    }
+	function drawNet()
+	{
+		for (let i = 0; i < canvas_height; i += 20)
+			drawRect(net.x, net.y + i, net.width, net.height, net.color);
+	}
 
-    function collision(ball:any, player:any)
-    {
-        ball.top = ball.y - ball.radius;
-        ball.bottom = ball.y + ball.radius;
-        ball.left = ball.x - ball.radius;
-        ball.right = ball.x + ball.radius;
+	function drawBall()
+	{
+		drawCircle(ball.x, ball.y, ball.radius, ball.color);
+	}
 
-        player.top = player.y;
-        player.bottom = player.y + player.height;
-        player.left = player.x;
-        player.right = player.x + player.width;
+	function drawScore()
+	{
+		drawText(player1.score.toString(), canvas_width / 4, canvas_height / 5, 'white');
+		drawText(player2.score.toString(), 3 * canvas_width / 4, canvas_height / 5, 'white');
+	}
 
-        return ball.right > player.left && ball.bottom > player.top && ball.left < player.right && ball.top < player.bottom;
-    }
+	function resetBall()
+	{
+		ball.x = canvas_width / 2;
+		ball.y = canvas_height / 2;
+		ball.speed = 5;
+		ball.velocityX = -ball.velocityX;
+		com.random = 0;
+	}
 
-    function render()
-    {
-        reset_canvas();
-        drawScore();
-        drawNet();
-        drawRect(user.x,user.y,user.width,user.height,user.color);
-        drawRect(com.x,com.y,com.width,com.height,com.color);
-        drawBall();
-    }
+	function movePaddle(event: MouseEvent)
+	{
 
-    function game()
-    {
+		let rect = canvasElement.getBoundingClientRect();
 
-        Update();
-        //UpdateClient();
-        render();
-        requestAnimationFrame(game);
-    }
+		$pongSocketStore.emit('onPaddleMove', {y: event.clientY - rect.top});
+
+		player1.paddle.y = event.clientY - rect.top - player1.paddle.height / 2;
+
+		if (player1.paddle.y < 0)
+			player1.paddle.y = 0;
+		else if (player1.paddle.y > canvas_height - player1.paddle.height)
+			player1.paddle.y = canvas_height - player1.paddle.height;
+	}
+
+	function Update()
+	{
+		// com.y = ball.y -  com.height/2 + com.random;
+
+		if (com.y + paddle_height > canvas_height)
+			com.y = canvas_height - paddle_height;
+		if (com.y < 0)
+			com.y = 0;
+
+		if (ball.x + ball.radius > canvas_width)
+		{
+			// user.score++;
+			$pongSocketStore.emit('onPlayerScored', {score: player1.score});
+			resetBall(); // reset so that next frame we dont accidentally resend message
+		}
+		else if (ball.x - ball.radius < 0)
+		{
+			// com.score++;
+
+			// opponent will emit itself so no need
+			// $pongSocketStore.emit('onOpponentScored', {score: com.score});
+
+			resetBall();
+		}
+
+		ball.x += ball.velocityX;
+		ball.y += ball.velocityY;
+
+		if (ball.y + ball.radius > canvas_height || ball.y - ball.radius < 0)
+		{
+			ball.velocityY = -ball.velocityY;
+		}
+
+
+		let player = (ball.x < canvas_width / 2) ? player1 : player2;
+		if (collision(ball, player.paddle))
+		{
+			let collisionPoint = ball.y - (player.paddle.y + player.paddle.height / 2);
+			collisionPoint = collisionPoint / (player.paddle.height / 2);
+
+			let angleRad = (Math.PI / 4) * collisionPoint;
+			let direction = (ball.x < canvas_width / 2) ? 1 : -1;
+			ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+			ball.velocityY = ball.speed * Math.sin(angleRad);
+			ball.speed += 0.2;
+		}
+	}
+
+	function collision(ball: IBall, paddle: IPaddle)
+	{
+		const ballTop = ball.y - ball.radius;
+		const ballBottom = ball.y + ball.radius;
+		const ballLeft = ball.x - ball.radius;
+		const ballRight = ball.x + ball.radius;
+
+		const paddleTop = paddle.y;
+		const paddleBottom = paddle.y + paddle.height;
+		const paddleLeft = paddle.x;
+		const paddleRight = paddle.x + paddle.width;
+
+		return ballLeft < paddleRight && ballTop < paddleBottom && ballRight > paddleLeft && ballBottom > paddleTop;
+	}
+
+	function render()
+	{
+		reset_canvas();
+		drawScore();
+		drawNet();
+		drawPaddle(player1.paddle);
+		drawPaddle(player2.paddle);
+		drawBall();
+	}
+
+	function loop()
+	{
+		Update();
+		render();
+		requestAnimationFrame(loop);
+	}
 
 </script>
 
