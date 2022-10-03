1b0e6f9dbb84f64d16d1d8bb4df69dc7186e4acd
Mederis Frambourt <mframbou@student.42nice.fr>
2022-10-03 21:34:44 +0200

    notification list working, need to add it to layout

--- frontend/src/lib/NotificationPopupList.svelte
+++ frontend/src/lib/NotificationPopupList.svelte
@@ -1,19 +1,44 @@
 <script lang="ts">
 
 	import NotificationPopup from '$lib/NotificationPopup.svelte';
-	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
 	let notifications = [];
+	let hiddenNotificationsIds: number[] = [];
+	let id: number = 0;
 
-	export function addNotification(notification) {
-		notifications.push(notification);
+	// only update when adding notif (not removing), otherwise if adding and removing at the same times it's buggy
+
+	export const addNotification = (notification) => {
+		const notifId = id++;
+		notifications.push({ content: notification, id: notifId });
+		notifications = notifications; // https://svelte.dev/tutorial/updating-arrays-and-objects
+	}
+
+	// only remove when nothing is shown to avoid bug (so when notifs match hidden notifs)
+	function removeNotification(id: number)
+	{
+		hiddenNotificationsIds.push(id);
+
+		if (notifications.length !== hiddenNotificationsIds.length)
+			return;
+
+		notifications = [];
 	}
 
 </script>
 
+
 <div class="wrapper">
 	<div class="notification-list">
-
+		{#each notifications.filter(n => !hiddenNotificationsIds.includes(n.id)) as notification (notification.id)}
+			<div class="notification" transition:slide animate:flip>
+					<NotificationPopup on:close={() => removeNotification(notification.id)} fixedPosition={false}>
+						{notification.content}
+					</NotificationPopup>
+			</div>
+		{/each}
 	</div>
 </div>
 
@@ -21,8 +46,19 @@
 
 	.notification-list
 	{
+		position: fixed;
+		bottom: 1rem;
+		right: 1rem;
 		display: flex;
 		flex-direction: column;
+		align-items: flex-end;
+		gap: .2rem;
+	}
+
+	.notification
+	{
+		//transition: all 2s ease-in-out;
+		width: 100%;
 	}
 
 </style>
--- frontend/src/routes/(authed)/test/+page.svelte
+++ frontend/src/routes/(authed)/test/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 
 	import { pongSocket, chatSocketConnected } from '$lib/websocket-stores';
-	import { onDestroy } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import NotificationPopup from "$lib/NotificationPopup.svelte";
+	import NotificationPopupList from "$lib/NotificationPopupList.svelte";
 
 	onDestroy(() => {
 		console.log('destroy');
@@ -14,10 +15,18 @@
 		}
 	})
 
+	let addNotification;
+
+	onMount(() => {
+		for (let i = 0; i < 10; i++)
+		{
+			setTimeout(() => {
+				const randomStrBetween10And20 = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+				addNotification(`Notification ${i}, ${randomStrBetween10And20}`);
+			}, 1000 * i + i * 10);
+		}
+	});
+
 </script>
 
-<NotificationPopup>
-	test<br>
-	hello everyone<br>
-	pouet
-</NotificationPopup>
+<NotificationPopupList bind:addNotification/>
